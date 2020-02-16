0c58560f7ef7d9fd9afd816fbb50701f83cf9905
Yang Ai <yangai@YangdeMacBook-Pro.local>
2020-02-16 02:33:23 -0500

    bug fixed

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,10 @@
 const alias = require('./alias.config').resolve.alias
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   'configureWebpack': {
     'resolve': {
       'alias': alias
-    },
-    plugins: [
-      new BundleAnalyzerPlugin()
-    ]
+    }
   },
   'css': {
     'sourceMap': true,
